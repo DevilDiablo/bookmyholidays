a200554a3ef3936bdff6bf533ad9ba1ef3069f56
DevilDiablo <46159189+DevilDiablo@users.noreply.github.com>
2021-06-05 18:34:33 +0530

    Update settings.py

--- bookmyholidat/settings.py
+++ bookmyholidat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8zvq@lpdqgpfpi!omwsxnv*8e@@%yhxp8t(i)ou3m(7lq=60o%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["bookmyholidays.herokuapp.com","127.0.0.1"]
 
