01b62b1d836a06978b263b96b6e57be94184f140
DevilDiablo <46159189+DevilDiablo@users.noreply.github.com>
2021-06-05 18:04:22 +0530

    Update settings.py

--- bookmyholidat/settings.py
+++ bookmyholidat/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static5')
 STATIC_URL = '/static5/'
 STATICFILES_DIRS = [
     BASE_DIR / "static5",
