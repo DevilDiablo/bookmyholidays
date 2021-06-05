115b880d6faa903c33d93ad240ac80cd421d7712
DevilDiablo <46159189+DevilDiablo@users.noreply.github.com>
2021-06-05 18:21:18 +0530

    Update settings.py

--- bookmyholidat/settings.py
+++ bookmyholidat/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static5/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static5",
-]
-
+STATIC_URL = "/STATIC/"
+STATIC_ROOT = OS.PATH.JOIN(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
